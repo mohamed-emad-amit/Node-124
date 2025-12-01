0f80f670b488b6ead42a7f8e2f415f831c3db905
MohamedamaD <mohamed.amad24483@gmail.com>
2025-12-01 20:44:50 +0200

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,5 @@ import { Profile } from "./pages/Profile";
 import { Home } from "./pages/Home";
 
 export default function App() {
-  return (
-    <div className="app-container">
-      <div className="navbar">
-        <Link to="/"> Home</Link>
-        <Link to="/profile"> Profile</Link>
-      </div>
-
-      <div className="main-container">
-        <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/profile" Component={Profile} />
-          <Route path="*" Component={NotFound} />
-        </Routes>
-      </div>
-    </div>
-  );
+  return <h1>Changes</h1>;
 }
