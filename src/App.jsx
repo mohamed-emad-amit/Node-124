69fb24288077975ff7c64c3a2650a71053d79398
MohamedamaD <mohamed.amad24483@gmail.com>
2025-12-01 20:52:26 +0200

    modify files or folder structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import { NotFound } from "./pages/NotFound";
-import { Profile } from "./pages/Profile";
-import { Home } from "./pages/Home";
-
-export default function App() {
-  return <h1>Changes</h1>;
-}
